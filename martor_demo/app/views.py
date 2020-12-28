be618aafd9f280ec65aa1b1500e7bb17ab5b6cff
Tomasz Dudziec <tomasz.dudziec@redembedded.com>
2020-12-28 23:28:25 +0100

    flake8 style fixes

--- martor_demo/app/views.py
+++ martor_demo/app/views.py
@@ -1,11 +1,9 @@
-from django.conf import settings
-from django.http import HttpResponse
-from django.shortcuts import (render, redirect)
-from django.contrib.auth.decorators import login_required
-from django.contrib import messages
-
 from app.forms import (SimpleForm, PostForm)
 from app.models import Post
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import (render, redirect)
 
 
 def home_redirect_view(request):
